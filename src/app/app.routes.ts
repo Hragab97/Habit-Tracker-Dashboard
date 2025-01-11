b133df5042e18597c477cb7c3b67d38756bd1117
Hragab97 <ragab1997@gmail.com>
2025-01-11 13:02:03 +0200

    [SCRUM-2] [HABIT-TRACKER-DASHBOARD] Create main project components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component'
+import { HabitsComponent } from './features/habits/habits.component'
+import { ReportsComponent } from './features/reports/reports/reports.component'
+import { SettingsComponent } from './features/settings/settings/settings.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent, title:'Dashboard' },
+  { path: 'habits', component: HabitsComponent, title: 'Habits' },
+  { path: 'reports',component: ReportsComponent, title: 'Reports' },
+  { path: 'settings', component: SettingsComponent, title: 'Settings' }
+  ];
